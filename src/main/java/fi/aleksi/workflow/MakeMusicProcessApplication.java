e5ae4a9f7a1b032bff3f26dc63912a438ab762a5
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2020-05-12 22:21:14 +0300

    Switched to Spring boot Camunda

--- src/main/java/fi/aleksi/workflow/MakeMusicProcessApplication.java
+++
@@ -1,8 +0,0 @@
-package fi.aleksi.workflow;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Make Music Application")
-public class MakeMusicProcessApplication extends ServletProcessApplication {
-}
--- src/main/java/fi/aleksi/workflow/MakeMusicWebApplication.java
+++
@@ -1,8 +0,0 @@
-package fi.aleksi.workflow;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class MakeMusicWebApplication extends SpringBootServletInitializer {
-}
